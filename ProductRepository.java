4b6d59a8f3a7e544eda561674a91c9a2ecd645e6
Afossofa <89802362+Afossofa@users.noreply.github.com>
2024-05-16 15:43:33 +0300

    repositiries

---
+++ ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.uniproject.repositories;
+
+import com.example.uniproject.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByTitle(String title);
+}
